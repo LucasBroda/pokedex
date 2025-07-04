58a1ef42a47f6e2d0930b600ce49c4a5f73d1f96
lucas_broda <lulubrod@gmail.com>
2025-07-04 18:32:44 +0100

    feat(pokemonRoutes): ajout d'une route dans l'api afin de recueillir le dernier ID

--- backend/src/routes/pokemonRoutes.js
+++ backend/src/routes/pokemonRoutes.js
@@ -7,5 +7,14 @@ router.get('/:id', getPokemonById); // Détails d'un Pokémon
 router.post('/', addPokemon); // Ajouter un Pokémon
 router.put('/:id', updatePokemon); // Modifier un Pokémon
 router.delete('/:id', deletePokemon); // Supprimer un Pokémon
+router.get('/last-id', async (req, res) => {
+    try {
+      const [rows] = await connection.query('SELECT MAX(id) AS lastId FROM pokemon');
+      res.json({ lastId: rows[0].lastId || 0 });
+    } catch (error) {
+      console.error('Erreur lors de la récupération du dernier ID :', error);
+      res.status(500).json({ message: 'Erreur serveur' });
+    }
+  });
 
 module.exports = router;
